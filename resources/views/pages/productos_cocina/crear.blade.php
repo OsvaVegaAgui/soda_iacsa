9058a52e83ad6ef62891daff38f690b86120f7ff
RichardGR2414 <richardgr.2426@gmail.com>
2025-11-06 18:19:57 -0600

    base ricardo

--- resources/views/pages/productos_cocina/crear.blade.php
+++ resources/views/pages/productos_cocina/crear.blade.php
@@ -1,1 +1,108 @@
-<h1>Ruta Ricardo</h1>
+@extends('layouts.master')
+
+@section('styles')
+
+    {{-- CSS PARA FECHA BONITA --}}
+    <link rel="stylesheet" href="{{asset('build/assets/libs/flatpickr/flatpickr.min.css')}}">
+
+    {{-- SELECT PARA BUSCAR --}}
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet">
+
+    <!-- CSS PARA VALIDACION DE CAMPOS -->
+    <link rel="stylesheet" href="{{asset('build/assets/libs/prismjs/themes/prism-coy.min.css')}}">
+
+    {{-- CSS PARA TABLAS --}}
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.bootstrap5.min.css">
+
+@endsection
+
+@section('content')
+	
+<div class="row">
+    <div class="col-xl-12">
+        <div class="card custom-card">
+            <div class="card-header justify-content-between">
+                <div class="card-title">
+                    Crear PRODUCTO-COCINA
+                </div>
+                <div class="prism-toggle">
+                    <button class="btn btn-sm btn-primary-light">Show Code<i class="ri-code-line ms-2 d-inline-block align-middle"></i></button>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label class="form-label">Nombre del Producto</label>
+                        <input type="text" class="form-control" placeholder="Nombre" aria-label="First name">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label class="form-label">Precio</label>
+                        <input type="number" class="form-control" placeholder="Ingrese el precio" aria-label="Phone number">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label class="form-label">Codigo softland</label>
+                        <input type="number" class="form-control" placeholder="Ingrese el codigo softland" aria-label="Phone number">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label class="form-label">Codigo de barras</label>
+                        <input type="number" class="form-control" placeholder="Ingrese el codigo de barras" aria-label="Phone number">
+                    </div>
+
+            
+                    <div class="col-md-6 mb-3">
+                        <div class="row">
+                            <div class="col-xl-12 mb-3">
+                                <div class="row">
+                                    <label class="form-label mb-1">Estado</label>
+                                    <div class="col-xl-6">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1"> Activo </label>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-6">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                                            <label class="form-check-label" for="flexRadioDefault2"> Inactivo </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+@endsection
+
+@section('scripts')
+        
+    {{-- JS PARA FECHA BONITA --}}
+    <script src="{{asset('build/assets/libs/flatpickr/flatpickr.min.js')}}"></script>
+    @vite('resources/assets/js/date&time_pickers.js')
+
+    {{-- JS PARA SELECT QUE BUSCA --}}
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    @vite('resources/assets/js/select2.js')
+
+    <!-- JS PARA VALIDACIONES -->
+    <script src="{{asset('build/assets/libs/prismjs/prism.js')}}"></script>
+    @vite('resources/assets/js/prism-custom.js')
+    @vite('resources/assets/js/validation.js')
+
+
+    {{-- jJS PARA TABLAS --}}
+    {{-- <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script> --}}
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.6/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+
+    @vite('resources/assets/js/datatables.js')
+
+@endsection
